c67b53348ece704fa6cfeb2a157073f34d0db3a7
Gowtham  H D <gowthambhat793@gmail.com>
2020-11-03 00:35:26 +0530

    first commit

---
+++ src/Greetings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greet = () => {
+    return <h1>hy gowtham</h1>
+}
+const AnotherGreet = () => {
+    return <h6>just a text</h6>
+}
+
+export { AnotherGreet };
+export default Greet
